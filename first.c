3ad2b881a5fa6d1e9504edc5591b00d668834be2
ram lende <ramlende91@gmail.com>
2024-09-10 14:29:07 +0530

    changes added

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+int main()
+{
+    int num1 , num2 ,sum;
+    printf("enter first nunmer:");
+    scanf("%d",&num1);
+    printf("enter second number:");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("sum:%d\n",sum);
+    return 0;
+}
---
+++ mypython.py
@@ -0,0 +1,7 @@
+num = int(input('Enter number'))
+count = 0
+while count > 0: 
+    num= num // 10
+    count = count + 1
+
+print('number count', count)  
